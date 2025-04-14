c6b0735dcd86d64e09f6aa2a21901a47d3dbb6c9
Benjamin Wong <me@benjiwong.com>
2025-04-13 23:39:58 -0700

    Adjusted starting code to render more accurately based on screen size

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <SDL2/SDL.h>
+#include <cmath>
 #include <iostream>
 #include <array>
 
 const int SCREEN_WIDTH = 128;
 const int SCREEN_HEIGHT = 128;
-const int WINDOW_SCALE = 4;
+const int WINDOW_SCALE = 5;
 
 const std::array<SDL_Color, 4> PALETTE = {{
    { 0, 0, 0, 255 },
@@ -19,9 +20,23 @@ void fill_varm_test() {
    for (int y = 0; y < SCREEN_HEIGHT; ++y) {
       for (int x = 0; x < SCREEN_WIDTH; ++x) {
          int index = y * SCREEN_WIDTH + x;
-         vram[index] = (x / 8 + y / 8) % 4;
+         vram[index] = (int)(x * sin(x) * 2 + 2 + y) % 4;
       }
    }
+}
+
+void update_frame_buffer(SDL_Texture *frame_buffer) {
+   uint32_t *pixels;
+   int pitch;
+   
+   SDL_LockTexture(frame_buffer, nullptr, (void**)&pixels, &pitch);
+   SDL_PixelFormat* format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
+   for (int i = 0; i < vram.size(); ++i) {
+      SDL_Color c = PALETTE[vram[i] % 4];
+      pixels[i] = SDL_MapRGBA(format, c.r, c.g, c.b, c.a);
+   }
+   SDL_FreeFormat(format);
+   SDL_UnlockTexture(frame_buffer);
 }
 
 int main(int argc, char* argv[]) {
@@ -30,17 +45,20 @@ int main(int argc, char* argv[]) {
       return 1;
    }
 
-   SDL_Window* window = SDL_CreateWindow(
-         "fido",
-         SDL_WINDOWPOS_CENTERED,
-         SDL_WINDOWPOS_CENTERED,
+   SDL_Event event;
+   SDL_Renderer *renderer;
+   SDL_Window *window;
+
+   SDL_Texture *frame_buffer;
+   bool running = true;
+
+   SDL_CreateWindowAndRenderer(
          SCREEN_WIDTH * WINDOW_SCALE,
          SCREEN_HEIGHT * WINDOW_SCALE,
-         SDL_WINDOW_SHOWN
+         0,
+         &window, &renderer
       );
-
-   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-   SDL_Texture* texture = SDL_CreateTexture(
+   frame_buffer = SDL_CreateTexture(
          renderer,
          SDL_PIXELFORMAT_RGBA8888,
          SDL_TEXTUREACCESS_STREAMING,
@@ -50,31 +68,23 @@ int main(int argc, char* argv[]) {
 
    fill_varm_test();
 
-   bool running = true;
-   SDL_Event event;
-
    while (running) {
       while (SDL_PollEvent(&event)) {
          if (event.type == SDL_QUIT) running = false;
       }
 
-      uint32_t* pixels;
-      int pitch;
-      SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch);
-      for (int i = 0; i < vram.size(); i++) {
-         SDL_Color c = PALETTE[vram[i] % 4];
-         pixels[i] = (c.a << 24) | (c.r << 16) | (c.g << 8) | c.b;
-      }
-      SDL_UnlockTexture(texture);
+      update_frame_buffer(frame_buffer);
 
       SDL_RenderClear(renderer);
-      SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+      //SDL_Rect dest = { 0, 0, SCREEN_WIDTH * WINDOW_SCALE, SCREEN_HEIGHT * WINDOW_SCALE };
+      SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+      SDL_RenderCopy(renderer, frame_buffer, nullptr, nullptr);
       SDL_RenderPresent(renderer);
 
       SDL_Delay(16);
    }
 
-   SDL_DestroyTexture(texture);
+   SDL_DestroyTexture(frame_buffer);
    SDL_DestroyRenderer(renderer);
    SDL_DestroyWindow(window);
    SDL_Quit();
